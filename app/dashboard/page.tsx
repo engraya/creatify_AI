ca6ce1a16d85e2668721ce59ca3f3fdcb347bf96
Engraya <ahmadyakubuaya@gmail.com>
2025-01-07 14:25:28 +0100

    Pushed aome files

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,1 +1,18 @@
-rfce
+"use client";
+
+import { useState } from "react";
+import { SearchDashboard } from "./_components/search-dashboard";
+import { TemplateList } from "./_components/template-list";
+
+const Dashboard = () => {
+  const [searchInput, setSearchInput] = useState<string>();
+
+  return (
+    <div>
+      <SearchDashboard onSearchInput={setSearchInput} />
+      <TemplateList searchInput={searchInput as string} />
+    </div>
+  );
+};
+
+export default Dashboard;
--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,19 @@
-import { PrismaClient } from "@prisma/client"
-import "server-only";
+// import { PrismaClient } from "@prisma/client"
+// import "server-only";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient
-}
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var cachedPrisma: PrismaClient
+// }
 
-export let prisma: PrismaClient
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
-  }
-  prisma = global.cachedPrisma
-}
+// export let prisma: PrismaClient
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.cachedPrisma) {
+//     global.cachedPrisma = new PrismaClient()
+//   }
+//   prisma = global.cachedPrisma
+// }
+import { PrismaClient } from "@prisma/client";
+export const db = new PrismaClient();
