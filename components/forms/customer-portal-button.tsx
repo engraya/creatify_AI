2d67244c081f03754f6bb7393b0ad5021864eb85
Engraya <ahmadyakubuaya@gmail.com>
2025-01-28 19:31:31 +0100

    Added some forms

--- components/forms/customer-portal-button.tsx
+++ components/forms/customer-portal-button.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useTransition } from "react";
-import { openCustomerPortal } from "@/actions/open-customer-portal";
-
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/shared/icons";
 
@@ -13,17 +10,10 @@ interface CustomerPortalButtonProps {
 export function CustomerPortalButton({
   userStripeId,
 }: CustomerPortalButtonProps) {
-  let [isPending, startTransition] = useTransition();
-  const generateUserStripeSession = openCustomerPortal.bind(null, userStripeId);
 
-  const stripeSessionAction = () =>
-    startTransition(async () => await generateUserStripeSession());
 
   return (
-    <Button disabled={isPending} onClick={stripeSessionAction}>
-      {isPending ? (
-        <Icons.spinner className="mr-2 size-4 animate-spin" />
-      ) : null}
+    <Button >
       Open Customer Portal
     </Button>
   );
